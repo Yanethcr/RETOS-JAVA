76fbbb7ae73cc1fdc784685054f143b6e4efbe0a
Yeni <139667423+Yanethcr@users.noreply.github.com>
2025-05-16 18:51:35 -0600

    Reto 1 Sesion 5

---
+++ Reto01_S5/src/Operador.java
@@ -0,0 +1,11 @@
+public class Operador {
+        String nombre;
+
+        public Operador(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public void reportarse() {
+            System.out.println("👷 Operador " + nombre + " reportándose.");
+        }
+}
